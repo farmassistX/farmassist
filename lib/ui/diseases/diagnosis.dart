cf1808e76959d0681548c93d608d3afb36647e87
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2021-01-06 23:53:11 +0800

    Create initial files for Disease Detection Page

---
+++ lib/ui/diseases/diagnosis.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/foundation.dart';
+
+class Diagnosis extends ChangeNotifier {}
---
+++ lib/ui/diseases/view_image_region.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewImageRegion extends StatefulWidget {
+  @override
+  _ViewImageRegionState createState() => _ViewImageRegionState();
+}
+
+class _ViewImageRegionState extends State<ViewImageRegion> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
